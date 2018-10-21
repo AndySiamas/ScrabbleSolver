ed64111e77c462fcc11517af08c91f072ef1750f
Andy Siamas <andys@Andys-iMac.local>
2018-10-21 16:48:32 -0700

    Updated combination algorithm

--- ScrabbleSolver.js
+++ ScrabbleSolver.js
@@ -1,34 +1,59 @@
-var ScrabbleSolver = function(letters) {
-  var letterCount = new LetterCount(letters);
-  console.log(this.getValidCombinations(letters, letterCount));
-}
+// LETTER COUNT
+class LetterCount {
+  constructor(letters) {
+    this.size = 0;
+    this.storage = {};
+    this.backup = {};
+    this.addLettersToCount(letters);
+  }
 
-ScrabbleSolver.prototype.isValidWord = function(word) {
-  return dictionary[word] === 1;
-}
-
-ScrabbleSolver.prototype.getValidCombinations = function(letters, _lettercount) {
-  var combinations = [];
-  var letterCount = _lettercount;
-
-  var getCombination = function(word = [], wordLength = 0) {
-    combinations.push(word);
-    
-    if (wordLength === letters.length) {
-      return;
-    }
-
-    letters.forEach(function (letter) {
-      if (letterCount.storage[letter] > 0) {
-        letterCount.storage[letter]--;
-        getCombination(word.concat(letter), wordLength+1);
+  addLettersToCount(letters) {
+    letters.forEach((letter) => {
+      this.size++;
+      if (this.storage.hasOwnProperty(letter)) {
+        this.storage[letter]++;
+      } else {
+        this.storage[letter] = 1;
       }
     });
   }
-
-  getCombination();
-
-  return combinations;
 }
 
-var solver = new ScrabbleSolver(['A', 'A', 'B', 'C']);
+// LETTER COUNT
+class ScrabbleSolver {
+  constructor(letters) {
+    this.letters = letters;
+    this.letterCount = new LetterCount(letters);
+    console.log(JSON.stringify(this.getValidCombinations(letters, this.letterCount)));
+    //console.log(this.isValidWord('hello'));
+  }
+
+  isValidWord(word) {
+    return dictionary[word] === 1;
+  }
+
+  getValidCombinations(_letters, _letterCount) {
+    const combinations = [];
+
+    const getCombinations = (_combo = []) => {
+      let word = _combo.join('');
+
+      if (this.isValidWord(word)) {
+        combinations.push(word);
+      }
+
+      _letters.forEach((letter) => {
+        if (_letterCount.storage[letter] > 0) {
+          _letterCount.storage[letter] --;
+          getCombinations(_combo.concat(letter));
+          _letterCount.storage[letter] ++;
+        }
+      });
+    }
+    
+    getCombinations([]);
+    return combinations;
+  }
+}
+
+var solver = new ScrabbleSolver(['t', 'a', 'p']);
